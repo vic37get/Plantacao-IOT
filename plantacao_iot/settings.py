2e3b029c23bf9da76a1d2cff12097f7b5ce599e0
Victor <victorribeiro172@gmail.com>
2022-09-20 16:50:32 -0300

    subindo no heroku

--- plantacao_iot/settings.py
+++ plantacao_iot/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kx@)m7ow3@%)%+k=7us80%!q*&l=7tz52n-$w16&b7j$*t8&3@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'plantacao.herokuapp.com']
 
 
 # Application definition
