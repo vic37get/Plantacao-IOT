38dc8fae96a659016e1e08f7c9a52c0eae0c28b7
Victor Ribeiro da Silva <51518489+vic37get@users.noreply.github.com>
2022-09-21 15:20:10 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,13 +3,12 @@ from bs4 import BeautifulSoup
 import re
 #geoposition currentconditions forecast
 
-API_KEY=''
-def previsão_tempo():
+requisiçao = 'https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi'
+
+def temperatura(requisição):
     exp = re.compile("\\<.*?\\>")
-    page = requests.get("https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi")
+    page = requests.get(requisição)
     soup = BeautifulSoup(page.content, 'html.parser')
     temperatura = soup.find_all('span', id='min-temp-1')
-    temperatura = re.sub(exp, '', temperatura)
-    print(temperatura)
-    return
-
+    temperatura = re.sub(exp, '', str(temperatura))
+    return temperatura
