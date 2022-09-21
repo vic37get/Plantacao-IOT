0769d6df6634da0fb44c5646f48cca7a7ac4aeaf
Victor Ribeiro da Silva <51518489+vic37get@users.noreply.github.com>
2022-09-21 15:22:17 -0300

    chamadas da API

--- plantacao_app/views.py
+++ plantacao_app/views.py
@@ -3,19 +3,19 @@ import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.template import loader
-from utils import previsão_tempo
+from utils import temperaturaMinima, temperaturaMaxima, umidadeMinima, umidadeMaxima
 
 
 # Create your views here.
 def home(request):
     template = loader.get_template('plantacao_app/home.html')
     context={
-
+        'temperatura_minima': temperaturaMinima(),
+        'temperatura_maxima': temperaturaMaxima(),
+        'umidade_minima': umidadeMinima(),
+        'umidade_maxima': umidadeMaxima()
     }
-    previsão_tempo()
     return HttpResponse(template.render(context, request))
 
 def led(request):
     return JsonResponse({'led': 'on'})
-
-
