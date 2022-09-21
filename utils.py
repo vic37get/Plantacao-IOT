ed78a7968d1bd3b6baa798807330fb8dcf6a1200
Victor Ribeiro da Silva <51518489+vic37get@users.noreply.github.com>
2022-09-21 15:20:49 -0300

    update utils

--- utils.py
+++ utils.py
@@ -3,12 +3,38 @@ from bs4 import BeautifulSoup
 import re
 #geoposition currentconditions forecast
 
-requisiçao = 'https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi'
-
-def temperatura(requisição):
+def temperaturaMinima():
+    request = "https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi"
     exp = re.compile("\\<.*?\\>")
-    page = requests.get(requisição)
+    page = requests.get(request)
     soup = BeautifulSoup(page.content, 'html.parser')
     temperatura = soup.find_all('span', id='min-temp-1')
-    temperatura = re.sub(exp, '', str(temperatura))
+    temperatura = re.sub(exp, '', str(temperatura[0]))
     return temperatura
+
+def temperaturaMaxima():
+    request = "https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi"
+    exp = re.compile("\\<.*?\\>")
+    page = requests.get(request)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    temperatura_maxima = soup.find_all('span', id='max-temp-1')
+    temperatura_maxima = re.sub(exp, '', str(temperatura_maxima[0]))
+    return temperatura_maxima
+
+def umidadeMinima():
+    request = "https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi"
+    exp = re.compile("\\<.*?\\>")
+    page = requests.get(request)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    umidade_minima = soup.find_all('span', class_='-gray-light')
+    umidade_minima = re.sub(exp, '', str(umidade_minima[2]))
+    return umidade_minima
+
+def umidadeMaxima():
+    request = "https://www.climatempo.com.br/previsao-do-tempo/cidade/264/teresina-pi"
+    exp = re.compile("\\<.*?\\>")
+    page = requests.get(request)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    umidade_maxima = soup.find_all('span', class_='-gray-light')
+    umidade_maxima = re.sub(exp, '', str(umidade_maxima[3]))
+    return umidade_maxima
