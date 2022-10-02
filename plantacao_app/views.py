310fc6703f2bc015109b5b1a14ad8264629dfbcb
Victor <victorribeiro172@gmail.com>
2022-10-02 11:29:48 -0300

    adição de estilos e do json de dados

--- plantacao_app/views.py
+++ plantacao_app/views.py
@@ -1,26 +1,23 @@
 import json
 
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
 from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
-from utils import temperaturaMinima, temperaturaMaxima, umidadeMinima, umidadeMaxima
-from utils import connectMongo
-from bson.objectid import ObjectId
+from utils import (temperaturaMaxima, temperaturaMinima, umidadeMaxima,
+                   umidadeMinima)
 
-db_client = connectMongo('PlantacaoIOT')
 
-# Create your views here.
 def home(request):
-    #collection_aplicacaoData = db_client['AplicacaoData']
-    #dados = collection_aplicacaoData.find_one({})
+    with open('informacoes.json', 'r') as f:
+        dados = json.load(f)
     template = loader.get_template('plantacao_app/home.html')
+    
     context={
-        #'temperatura_dht11': dados['temperatura'],
-        #'chuva': dados['chuva'],
-        #'onOff': dados['onOff'],
-        #'umidade_dht11': dados['umidadeAr'],
-        #'umidade_solo': dados['umidadeSolo'],
+        'temperatura_dht11': dados['temperatura'],
+        'chuva': dados['chuva'],
+        'status': dados['status'],
+        'umidade_dht11': dados['umidadeAr'],
+        'umidade_solo': dados['umidadeSolo'],
         'temperatura_minima': temperaturaMinima(),
         'temperatura_maxima': temperaturaMaxima(),
         'umidade_minima': umidadeMinima(),
@@ -28,14 +25,11 @@ def home(request):
     }
     return HttpResponse(template.render(context, request))
 
-def led(request):
-    return JsonResponse({'led': 'on'})
-
 @csrf_exempt
 def recebe_informacoes(request):
-    #collection_AplicacaoData = db_client['AplicacaoData']
-    dados = request.GET
-    #registro_banco = collection_AplicacaoData.find_one({})
-    #collection_AplicacaoData.update_one({'_id': ObjectId(registro_banco['_id'])},{'$set':dados},upsert=True)
-    response_data = dados
+    response_data = request.GET
+    json_dados = json.dumps(response_data, indent=4)
+    with open('informacoes.json', 'w') as f:
+        f.write(json_dados)
+    print(response_data)
     return HttpResponse(json.dumps(response_data), content_type="application/json")
