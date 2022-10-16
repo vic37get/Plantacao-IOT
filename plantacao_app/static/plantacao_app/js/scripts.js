dfdb564e60224b0787e2ec10b5b888461842e1e5
Victor <victorribeiro172@gmail.com>
2022-10-16 17:40:01 -0300

    adição da alteração dos dados para categóricos

--- plantacao_app/static/plantacao_app/js/scripts.js
+++ plantacao_app/static/plantacao_app/js/scripts.js
@@ -1,5 +1,16 @@
 function showMetricsData(metrica, limiar, alvo, classe1, classe2){
-    if(metrica>=limiar){
+    if(metrica >= limiar){
+        element = document.getElementById(alvo)
+        element.setAttribute('class', classe1);
+    }
+    else{
+        element = document.getElementById(alvo)
+        element.setAttribute('class', classe2);
+    }
+}
+
+function showMetricsDataForTerm(metrica, limiar, alvo, classe1, classe2){
+    if(metrica == limiar){
         element = document.getElementById(alvo)
         element.setAttribute('class', classe1);
     }
